d7c5d572b04055ea56419ac9cd70f55fbdd0ca03
Erick <erick.araujo98@hotmail.com>
2019-07-01 11:57:44 -0300

    Começando a colocar pecas no tabuleiro

---
+++ xadrez_console/tabuleiro/Peca.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; protected set; }
+        public int QteMovimentos { get; protected set; }
+        public Tabuleiro Tab { get; protected set; }
+
+        public Peca(Tabuleiro tab, Cor cor)
+        {
+            Posicao = null;
+            Tab = tab;
+            Cor = cor;
+            QteMovimentos = 0;
+
+
+
+        }
+
+
+    }
+}
---
+++ xadrez_console/tabuleiro/Tabuleiro.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    class Tabuleiro
+    {
+
+        public int Linhas { get; set; }
+        public int Colunas { get; set; }
+        private Peca[,] Pecas;
+
+        public Tabuleiro(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+            Pecas = new Peca[linhas, colunas];
+        }
+
+
+        public Peca peca(int linha, int coluna)
+        {
+            return Pecas[linha, coluna];
+        }
+
+        public Peca peca(Posicao pos)
+        {
+            return Pecas[pos.Linha,pos.Coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+
+            validarPosicao(pos);
+
+            if(pos != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        public void colocarPeca(Peca p, Posicao pos)
+        {
+            Pecas[pos.Linha, pos.Coluna] = p;
+            p.Posicao = pos;
+
+        }
+
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.Linha < 0 || pos.Linha > Linhas || pos.Coluna < 0 || pos.Coluna > Colunas)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
+    }
+}
